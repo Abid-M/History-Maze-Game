c6c318440848f85efc314bdc3c0aea455cd632e2
Abid <abid1702@outlook.com>
2023-11-03 14:51:35 +0000

    add logger

--- server/index.js
+++ server/index.js
@@ -5,15 +5,13 @@ const logger = require("./logger");
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(logger)
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}.`);
 })
-
-// app.use(logger);
-
 
 app.get('/', (req, res) => {
     res.send(`Welcome to team 3 project!`);
