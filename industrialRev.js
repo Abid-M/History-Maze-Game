5d3a91531c0d4bbe008f8f137b26de43ffcd9ce0
Abid Miah <ec20298@qmul.ac.uk>
2023-11-07 09:28:46 +0000

    landing, enpoint

---
+++ industrialRev.js
@@ -0,0 +1,13 @@
+const industrialQs = [
+  {
+      question: "Can you name one famous medieval queen of England?",
+      answers: [
+          {text: "Anne Boleyn", value: 0},
+          {text: "Queen Elizabeth II", value: 0},
+          {text: "Queen Victoria", value: 1},
+          {text: "Diana", value: 0},
+      ]
+  },
+
+
+]
